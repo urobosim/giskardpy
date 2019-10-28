6d36be68e926608780ebf4989536c3fbb8ba7f11
Simon Stelter <stelter@uni-bremen.de>
2019-10-28 17:40:46 +0100

    casadi qp solver

--- src/giskardpy/qp_solver.py
+++ src/giskardpy/qp_solver.py
@@ -2,13 +2,14 @@ import numpy as np
 
 import qpoases
 from qpoases import PyReturnValue
+import casadi as ca
 
 from giskardpy.exceptions import MAX_NWSR_REACHEDException, QPSolverException
 from giskardpy import logging
 
 
 class QPSolver(object):
-    RETURN_VALUE_DICT = {value: name for name, value in vars(PyReturnValue).items()}
+    # RETURN_VALUE_DICT = {value: name for name, value in vars(PyReturnValue).items()}
 
     def __init__(self, dim_a, dim_b):
         """
@@ -17,16 +18,41 @@ class QPSolver(object):
         :param dim_b: number of hard constraints + number of soft constraints
         :type int
         """
-        self.qpProblem = qpoases.PySQProblem(dim_a, dim_b)
-        options = qpoases.PyOptions()
-        options.setToMPC()
-        options.printLevel = qpoases.PyPrintLevel.NONE
-        self.qpProblem.setOptions(options)
-        self.xdot_full = np.zeros(dim_a)
 
+        # self.qpProblem = qpoases.PySQProblem(dim_a, dim_b)
+        # options = qpoases.PyOptions()
+        # options.setToMPC()
+        # options.printLevel = qpoases.PyPrintLevel.NONE
+        # self.qpProblem.setOptions(options)
+        # self.xdot_full = np.zeros(dim_a)
+        #
         self.started = False
 
     @profile
+    def init_problem(self, H, g, A, lb, ub, lbA, ubA, nWSR=None):
+        qp = {}
+        qp['h'] = ca.DM(H).sparsity()
+        qp['a'] = ca.DM(A).sparsity()
+        opts = {'verbose': False,
+                'print_time': False,
+                'print_info':False,
+                'print_header':False,
+                'print_iter':False}
+        self.qp_problem = ca.conic('S', 'qrqp', qp, opts)
+        # opts = {'verbose': False,
+        #         'print_time': False,
+        #         'printLevel': 'none',
+        #         }
+        # self.qp_problem = ca.conic('S', 'qpoases', qp, opts)
+        self.started = True
+        return self.hot_start(H, g, A, lb, ub, lbA, ubA, nWSR)
+
+    @profile
+    def hot_start(self, H, g, A, lb, ub, lbA, ubA, nWSR=None):
+        r = self.qp_problem(h=H, g=g, a=A, lba=lbA, uba=ubA, lbx=lb, ubx=ub)
+        x_opt = np.array(r['x'])
+        return x_opt.T
+
     def solve(self, H, g, A, lb, ub, lbA, ubA, nWSR=None):
         """
         x^T*H*x + x^T*g
@@ -51,61 +77,65 @@ class QPSolver(object):
         :return: x according to the equations above, len = joint constraints + soft constraints
         :type np.array
         """
-        number_of_retries = 2
-        while number_of_retries > 0:
-            if nWSR is None:
-                nWSR = np.array([sum(A.shape) * 2])
-            else:
-                nWSR = np.array([nWSR])
-            number_of_retries -= 1
-            if not self.started:
-                success = self.qpProblem.init(H, g, A, lb, ub, lbA, ubA, nWSR)
-                if success == PyReturnValue.MAX_NWSR_REACHED:
-                    self.started = False
-                    raise MAX_NWSR_REACHEDException(u'Failed to initialize QP-problem.')
-            else:
-                success = self.qpProblem.hotstart(H, g, A, lb, ub, lbA, ubA, nWSR)
-                if success == PyReturnValue.MAX_NWSR_REACHED:
-                    self.started = False
-                    raise MAX_NWSR_REACHEDException(u'Failed to hot start QP-problem.')
-            if success == PyReturnValue.SUCCESSFUL_RETURN:
-                self.started = True
-                break
-            elif success == PyReturnValue.NAN_IN_LB:
-                # TODO nans get replaced with 0 document this somewhere
-                # TODO might still be buggy when nan occur when the qp problem is already initialized
-                lb[np.isnan(lb)] = 0
-                nWSR = None
-                self.started = False
-                number_of_retries += 1
-                continue
-            elif success == PyReturnValue.NAN_IN_UB:
-                ub[np.isnan(ub)] = 0
-                nWSR = None
-                self.started = False
-                number_of_retries += 1
-                continue
-            elif success == PyReturnValue.NAN_IN_LBA:
-                lbA[np.isnan(lbA)] = 0
-                nWSR = None
-                self.started = False
-                number_of_retries += 1
-                continue
-            elif success == PyReturnValue.NAN_IN_UBA:
-                ubA[np.isnan(ubA)] = 0
-                nWSR = None
-                self.started = False
-                number_of_retries += 1
-                continue
-            else:
-                logging.loginfo(u'{}; retrying with A rounded to 5 decimal places'.format(self.RETURN_VALUE_DICT[success]))
-                r = 5
-                A = np.round(A, r)
-                nWSR = None
-                self.started = False
-        else:  # if not break
-            self.started = False
-            raise QPSolverException(self.RETURN_VALUE_DICT[success])
-
-        self.qpProblem.getPrimalSolution(self.xdot_full)
-        return self.xdot_full
+        if not self.started:
+            return self.init_problem(H, g, A, lb, ub, lbA, ubA)
+        else:
+            return self.hot_start(H, g, A, lb, ub, lbA, ubA)
+        # number_of_retries = 2
+        # while number_of_retries > 0:
+        #     if nWSR is None:
+        #         nWSR = np.array([sum(A.shape) * 2])
+        #     else:
+        #         nWSR = np.array([nWSR])
+        #     number_of_retries -= 1
+        #     if not self.started:
+        #         success = self.init_problem(H, g, A, lb, ub, lbA, ubA, nWSR)
+        #         if success == PyReturnValue.MAX_NWSR_REACHED:
+        #             self.started = False
+        #             raise MAX_NWSR_REACHEDException(u'Failed to initialize QP-problem.')
+        #     else:
+        #         success = self.qpProblem.hotstart(H, g, A, lb, ub, lbA, ubA, nWSR)
+        #         if success == PyReturnValue.MAX_NWSR_REACHED:
+        #             self.started = False
+        #             raise MAX_NWSR_REACHEDException(u'Failed to hot start QP-problem.')
+        #     if success == PyReturnValue.SUCCESSFUL_RETURN:
+        #         self.started = True
+        #         break
+        #     elif success == PyReturnValue.NAN_IN_LB:
+        #         # TODO nans get replaced with 0 document this somewhere
+        #         # TODO might still be buggy when nan occur when the qp problem is already initialized
+        #         lb[np.isnan(lb)] = 0
+        #         nWSR = None
+        #         self.started = False
+        #         number_of_retries += 1
+        #         continue
+        #     elif success == PyReturnValue.NAN_IN_UB:
+        #         ub[np.isnan(ub)] = 0
+        #         nWSR = None
+        #         self.started = False
+        #         number_of_retries += 1
+        #         continue
+        #     elif success == PyReturnValue.NAN_IN_LBA:
+        #         lbA[np.isnan(lbA)] = 0
+        #         nWSR = None
+        #         self.started = False
+        #         number_of_retries += 1
+        #         continue
+        #     elif success == PyReturnValue.NAN_IN_UBA:
+        #         ubA[np.isnan(ubA)] = 0
+        #         nWSR = None
+        #         self.started = False
+        #         number_of_retries += 1
+        #         continue
+        #     else:
+        #         logging.loginfo(u'{}; retrying with A rounded to 5 decimal places'.format(self.RETURN_VALUE_DICT[success]))
+        #         r = 5
+        #         A = np.round(A, r)
+        #         nWSR = None
+        #         self.started = False
+        # else:  # if not break
+        #     self.started = False
+        #     raise QPSolverException(self.RETURN_VALUE_DICT[success])
+        #
+        # self.qpProblem.getPrimalSolution(self.xdot_full)
+        # return self.xdot_full
